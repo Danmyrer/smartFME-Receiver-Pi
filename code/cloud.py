8771b6c7a6db3b7cbbe8e9346f06fbf49a3a81a6
Danmyrer <dangwrb.09@gmail.com>
2022-06-06 17:36:22 +0100

    Initial preProcess.py

--- code/cloud.py
+++ code/cloud.py
@@ -4,25 +4,25 @@ from azure.iot.device.aio import IoTHubDeviceClient
 
 import key
 
-class Azure:
+class Cloud:
     def __init__(self):
         try:
             self.client = IoTHubDeviceClient.create_from_connection_string(key.AZURE_CONNECTION_STRING)
         except Exception as error:
             print(error.args[0])
-        print("[Azure] Client connected successfully!")
+        print("[Cloud] Client connected successfully!")
 
     async def push_payload(self, payload):
         message = Message(payload)
-        print(f"[Azure] Sending message: {message}")
+        print(f"[Cloud] Sending message: {message}")
         await self.client.send_message(message)
-        print("[Azure] Message successfully sent!")
+        print("[Cloud] Message successfully sent!")
 
 #region debug
 def test(dummy):
     payload = '{"A_DATA":"' + dummy + '"}'
-    test_Azure = Azure()
-    asyncio.run(test_Azure.push_payload(payload))
+    test_Cloud = Cloud()
+    asyncio.run(test_Cloud.push_payload(payload))
 
 test("Gruppenruf-1-Weinheim-Einsatz B2.2 LKW-Brand/A61/A61 => Koblenz 316.2 - 318.2, 316.3 - 318.2/54 AK Alzey => 53 Bornheim///19:00:06 2B 19:51 10.Feb")
 #endregion
