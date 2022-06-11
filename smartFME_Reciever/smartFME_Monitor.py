df8ddbea059f96d82ff3e100dfd16f4da5ccb836
Danmyrer <dangwrb.09@gmail.com>
2022-06-11 22:28:02 +0100

    Test #10

--- smartFME_Reciever/smartFME_Monitor.py
+++ smartFME_Reciever/smartFME_Monitor.py
@@ -2,6 +2,7 @@ import asyncio
 from smartFME_Reciever import payload as pl
 from smartFME_Reciever import cloud
 
+
 def einsatz(einsatzbeschreibung=False):
     if einsatzbeschreibung is False:
         payload = pl.buildPayload()
